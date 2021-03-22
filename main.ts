8a5734079c6f924bbe41d0f190ee11c09ce6822b
luca11234 <luca.it@gmail.com>
2021-03-22 15:11:37 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("LILLIANNA IS A FANTASTIK SINGER ")
+    basic.showString("LILLYANNA IS VERY LAZY STUDENT ")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("SHE CAN SING 3 + 3 = ")
 })
-music.playMelody("- G E D E G - - ", 120)
+music.playMelody("C5 A B G A F G E ", 120)
 basic.forever(function () {
     basic.showLeds(`
         . . . . .
